1f543527c340e0512b98805e9db152145153ec7a
Bosco So <bosco@indiegogo.com>
2012-10-24 20:44:01 -0700

    initial Rails 3.2.6 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theraffler::Application.config.secret_token = 'f961a3e24a53eb8cc9cd44c2623f66f80078fe39b1d32ffa343b67a912f0adec5f91129bba6dd2ee22b52ea66bb5b3512f7b871c42f041e73a2d1b8f22c59d18'
